55629727a79e5d2aa05eefbff66c26d3a3662367
Kate McColl <kate@katemccollart.com>
2021-06-29 11:50:08 +1000

    add: api route

--- Develop/server.js
+++ Develop/server.js
@@ -15,6 +15,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout-tracker
     useFindAndModify: false
 });
 
+app.use(require("./routes/api"));
+
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`)
 });
