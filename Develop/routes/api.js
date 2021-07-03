e3b9c2e3ffb0b164c4445a720bd165adf74b4eea
Kate McColl <kate@katemccollart.com>
2021-07-04 02:43:47 +1000

    add:  curly barkets to Workout

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const Workout = require("../models/workout");
+const { Workout } = require("../models");
 
 router.get("/api/workouts", (req, res) => {
     Workout.find({})
